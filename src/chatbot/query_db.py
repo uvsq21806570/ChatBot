5b9f5c721d85f8e40c1dcccd7977597056f8c2fd
uvsq21806570 <matteo.giraudo@ens.uvsq.fr>
2022-04-16 18:17:18 +0200

    Ajout de requêtes

--- src/chatbot/query_db.py
+++ src/chatbot/query_db.py
@@ -1,3 +1,5 @@
+from calendar import weekday
+import calendar
 import datetime as dt
 import pyodbc
 
@@ -8,19 +10,22 @@ password = "uvsq21806570Tostaky78_"
 driver = "{ODBC Driver 18 for SQL Server}"
 
 NOW = "DATEDIFF(second, '19700101', sysutcdatetime())"
+HOUR = "DATEPART(hour, (DATEADD(second, convert(INT, timestamp), '1970-01-01')))"
+DAY = "DATEPART(weekday, (DATEADD(second, convert(INT, timestamp), '1970-01-01')))"
 
 
 def current_data(row) :
-    print(str(row[0]) + " " + str(row[2]) + " " + str(row[3]) + " " + str(row[4]) + " " 
+    return (str(row[0]) + " " + str(row[2]) + " " + str(row[3]) + " " + str(row[4]) + " " 
         + str(row[5]) + " " + str(row[6]) + " " + str(row[7]) + " " + str(row[8]) + " " 
         + str(row[9]) + " " + str(row[10])
     )
+    
 
+# tag = function name
 
-# Quelles sont les données les plus récentes ?
-# Quelle est la pollution actuelle en France ?
-# Que renseigne les dernières collectes de données ?
-# tag : recent_news
+# What is the pollution of the last few minutes in France ?
+# What is the current air quality in each city ?
+# Give me the live pollution rate in the whole France.
 def recent_news(cursor) :
     request = "SELECT * FROM Pollution WHERE timestamp <= " + NOW + " AND timestamp > " + NOW + " - 3600"
     cursor.execute(request)
@@ -29,43 +34,133 @@ def recent_news(cursor) :
         current_data(row)
         row = cursor.fetchone()
 
-# A quel moment de la journée atteint t'on le pic de pollution ?
-# A quelle heure la pollution est-elle la plus élevée ?
-# tag : max_pollution_hour
-def max_pollution_hour(cursor) :
-    hour = "DATEPART(hour, (DATEADD(second, convert(INT, timestamp), '1970-01-01')))"
-    avg_pollution_hour = "(SELECT " + hour + " hour, AVG(aqi) aqi FROM Pollution GROUP BY " + hour + ") P"
-    request = "SELECT hour FROM " + avg_pollution_hour + " INNER JOIN (SELECT MAX(aqi) aqi FROM " + avg_pollution_hour + ") M ON (P.aqi = M.aqi)"
-    cursor.execute(request)
-    row = cursor.fetchone()
-    while row:
-        print("Le pic de pollution est souvent atteint vers " + str(row[0]) + " heures.")
-        row = cursor.fetchone()
 
-# A quel moment de la journée est t'il le moins risqué de sortir ?
-# A quelle heure la pollution est-elle la moins élevée ?
-# tag : min_pollution_hour
-def min_pollution_hour(cursor) :
-    hour = "DATEPART(hour, (DATEADD(second, convert(INT, timestamp), '1970-01-01')))"
-    avg_pollution_hour = "(SELECT " + hour + " hour, AVG(aqi) aqi FROM Pollution GROUP BY " + hour + ") P"
-    request = "SELECT hour FROM " + avg_pollution_hour + " INNER JOIN (SELECT MIN(aqi) aqi FROM " + avg_pollution_hour + ") M ON (P.aqi = M.aqi)"
-    cursor.execute(request)
-    row = cursor.fetchone()
-    while row:
-        print("La pollution est au plus faible vers " + str(row[0]) + " heures.")
-        row = cursor.fetchone()
-
-# Quelles sont les villes les moins polluées ?
-# Quelles villes sont les plus polluées ?
-# Quel est le classement des villes en fonction de leur taux de pollution ?
-def city_ranking(cursor) :
+# Which are the least polluted cities ?
+# Which cities are the most polluted ?
+# Which are the most polluted locations ?
+# Which locations are the least polluted ?
+# How do cities rank according to their pollution levels ? 
+def loc_ranking(cursor) :
     request = "SELECT loc, AVG(aqi) aqi FROM Pollution GROUP BY loc ORDER BY aqi DESC"
     cursor.execute(request)
     row = cursor.fetchone()
     while row:
-        print(str(row[0]))
+        print(str(row[0]) + " : " + str(row[1]))
         row = cursor.fetchone()
 
+
+# Ville la plus polluée
+# Ville la moins polluée
+
+
+# What is the air quality index according to the time of day ?
+# Does air quality change during the day ?
+# Is air quality worse in morning or evening ?
+# Is air quality worse during the day ?
+def hour_ranking(cursor) :
+    request = "SELECT " + HOUR + " hour, AVG(aqi) aqi FROM Pollution GROUP BY " + HOUR + " ORDER BY hour"
+    cursor.execute(request)
+    row = cursor.fetchone()
+    while row:
+        print(str(row[0]) + " heures : " + str(row[1]))
+        row = cursor.fetchone()
+
+
+# What is the air quality index according to the day of the week ?
+# Does air quality change over the week ?
+# Is air quality worse during the week-end or not ?
+def day_ranking(cursor) :
+    request = "SELECT " + DAY + " day, AVG(aqi) aqi FROM Pollution GROUP BY " + DAY + " ORDER BY day"
+    cursor.execute(request)
+    row = cursor.fetchone()
+    while row:
+        print(str(row[0]) + " " + calendar.day_name[int(row[0]) - 1] + " : " + str(row[1]))
+        row = cursor.fetchone()
+
+# At what time of day do we reach the pollution peak?
+# At what time is the pollution highest?
+# At what time is the air very bad ?
+# At what time of day can it be dangerous to go outside due to poor air quality?
+# tag : max_pollution_hour
+def max_pollution_hour(cursor) :
+    avg_pollution_hour = "(SELECT " + HOUR + " hour, AVG(aqi) aqi_h FROM Pollution GROUP BY " + HOUR + ") P"
+    request = "SELECT hour, aqi_h FROM " + avg_pollution_hour + " INNER JOIN (SELECT MAX(aqi_h) aqi FROM " + avg_pollution_hour + ") M ON (aqi_h = aqi)"
+    cursor.execute(request)
+    row = cursor.fetchone()
+    while row:
+        print("Le pic de pollution est souvent atteint vers " + str(row[0]) + " heures avec un AQI de " + str(row[1]))
+        row = cursor.fetchone()
+
+
+# What time of day is air quality the best ?
+# At what time of day is it least risky to go out ? 
+# At what time is the air good ?
+# What time is the lowest pollution ?
+# tag : min_pollution_hour
+def min_pollution_hour(cursor) :
+    avg_pollution_hour = "(SELECT " + HOUR + " hour, AVG(aqi) aqi_h FROM Pollution GROUP BY " + HOUR + ") P"
+    request = "SELECT hour, aqi_h FROM " + avg_pollution_hour + " INNER JOIN (SELECT MIN(aqi_h) aqi FROM " + avg_pollution_hour + ") M ON (aqi_h = aqi)"
+    cursor.execute(request)
+    row = cursor.fetchone()
+    while row:
+        print("La pollution est au plus faible vers " + str(row[0]) + " heures avec un AQI de " + str(row[1]))
+        row = cursor.fetchone()
+
+
+# What is the most polluted day ?
+# Which day is more polluted than the others?
+# On what day is the air quality worst?
+def max_pollution_day(cursor) :
+    avg_pollution_day = "(SELECT " + DAY + " day, AVG(aqi) aqi_d FROM Pollution GROUP BY " + DAY + ") P"
+    request = "SELECT day, aqi_d FROM " + avg_pollution_day + " INNER JOIN (SELECT MAX(aqi_d) aqi FROM " + avg_pollution_day + ") M ON (aqi_d = aqi)"
+    cursor.execute(request)
+    row = cursor.fetchone()
+    while row:
+        print("Le pic de pollution est souvent atteint le " + calendar.day_name[int(row[0]) - 1] + " avec un AQI de " + str(row[1]))
+        row = cursor.fetchone()
+
+
+# What is the least polluted day ?
+# Which day is less polluted than the others?
+# On what day is the air quality good?
+def min_pollution_day(cursor) :
+    avg_pollution_day = "(SELECT " + DAY + " day, AVG(aqi) aqi_d FROM Pollution GROUP BY " + DAY + ") P"
+    request = "SELECT day, aqi_d FROM " + avg_pollution_day + " INNER JOIN (SELECT MIN(aqi_d) aqi FROM " + avg_pollution_day + ") M ON (aqi_d = aqi)"
+    cursor.execute(request)
+    row = cursor.fetchone()
+    while row:
+        print("La pollution est au plus faible le " + calendar.day_name[int(row[0]) - 1] + " avec un AQI de " + str(row[1]))
+        row = cursor.fetchone()
+
+
+# Where and when is it highly inadvisable to go out ?
+# Where and when can it be very dangerous to leave your home
+# What are the coordinates and time frame of the poor air quality index ?
+def wrong_place_wrong_time(cursor) :
+    request = "SELECT DISTINCT loc, " + HOUR + " hour, AVG(aqi) aqi FROM Pollution GROUP BY loc, " + HOUR + " HAVING AVG(aqi) >= 3"
+    cursor.execute(request)
+    row = cursor.fetchone()
+    while row:
+        print(str(row[0]) + " à " + str(row[1]) + " heures : " + str(row[2]))
+        row = cursor.fetchone()
+
+
+# When and where was the pollution level very bad and potentially deadly ?
+# Where and when the air quality index was extremely high ?
+# Where and when was it very dangerous to leave your home ?
+def wrong_place_wrong_date(cursor) :
+    request = "SELECT  loc, timestamp, aqi FROM Pollution WHERE aqi = 5"
+    cursor.execute(request)
+    row = cursor.fetchone()
+    print("AQI = 5 :")
+    while row:
+        date = dt.datetime.fromtimestamp(int(row[1]))
+        print(str(row[0]) + " -> " + str(date))
+        row = cursor.fetchone()
+
+
+# What is the current pollution in Versailles
+# What is the air quality in Versailles in the last few minutes?
 def versailles_current_pollution(cursor) :
     request = "SELECT * FROM Pollution WHERE loc = 'Versailles' AND timestamp <= " + NOW + " AND timestamp > " + NOW + " - 3600"
     cursor.execute(request)
@@ -73,6 +168,7 @@ def versailles_current_pollution(cursor) :
     while row:
         current_data(row)
         row = cursor.fetchone()
+
 
 def lille_current_pollution(cursor) :
     request = "SELECT * FROM Pollution WHERE loc = 'Lille' AND timestamp <= " + NOW + " AND timestamp > " + NOW + " - 3600"
@@ -82,6 +178,7 @@ def lille_current_pollution(cursor) :
         current_data(row)
         row = cursor.fetchone()
 
+
 def nice_current_pollution(cursor) :
     request = "SELECT * FROM Pollution WHERE loc = 'Nice' AND timestamp <= " + NOW + " AND timestamp > " + NOW + " - 3600"
     cursor.execute(request)
@@ -89,6 +186,7 @@ def nice_current_pollution(cursor) :
     while row:
         current_data(row)
         row = cursor.fetchone()
+
 
 def brest_current_pollution(cursor) :
     request = "SELECT * FROM Pollution WHERE loc = 'Brest' AND timestamp <= " + NOW + " AND timestamp > " + NOW + " - 3600"
@@ -98,6 +196,7 @@ def brest_current_pollution(cursor) :
         current_data(row)
         row = cursor.fetchone()
 
+
 def bayonne_current_pollution(cursor) :
     request = "SELECT * FROM Pollution WHERE loc = 'Bayonne' AND timestamp <= " + NOW + " AND timestamp > " + NOW + " - 3600"
     cursor.execute(request)
@@ -106,6 +205,7 @@ def bayonne_current_pollution(cursor) :
         current_data(row)
         row = cursor.fetchone()
 
+
 def strasbourg_current_pollution(cursor) :
     request = "SELECT * FROM Pollution WHERE loc = 'Strasbourg' AND timestamp <= " + NOW + " AND timestamp > " + NOW + " - 3600"
     cursor.execute(request)
@@ -113,6 +213,7 @@ def strasbourg_current_pollution(cursor) :
     while row:
         current_data(row)
         row = cursor.fetchone()
+
 
 if __name__ == "__main__":
     with pyodbc.connect(
@@ -128,8 +229,4 @@ if __name__ == "__main__":
         + password
     ) as conn:
         with conn.cursor() as cursor:
-            max_pollution_hour(cursor)
-            min_pollution_hour(cursor)
-            versailles_current_pollution(cursor)   
-    
-# date = dt.datetime.fromtimestamp(int(row[1]))
+            wrong_place_wrong_date(cursor)
--- src/chatbot/train.py
+++ src/chatbot/train.py
@@ -9,18 +9,18 @@ from torch.utils.data import Dataset, DataLoader
 from nltk_utils import bag_of_words, tokenize, stem
 from model import NeuralNet
 
-with open('intents.json', 'r') as f:
+with open("intents.json", "r") as f:
     intents = json.load(f)
 
 all_words = []
 tags = []
 xy = []
 # loop through each sentence in our intents patterns, lire champs par champ
-for intent in intents['intents']:
-    tag = intent['tag']
+for intent in intents["intents"]:
+    tag = intent["tag"]
     # add to tag list
     tags.append(tag)
-    for pattern in intent['patterns']:
+    for pattern in intent["patterns"]:
         # tokenize each word in the sentence
         w = tokenize(pattern)
         # add to our words list
@@ -29,41 +29,41 @@ for intent in intents['intents']:
         xy.append((w, tag))
 
 # stem and lower each word
-ignore_words = ['?', '.', '!']
+ignore_words = ["?", ".", "!"]
 all_words = [stem(w) for w in all_words if w not in ignore_words]
 # remove duplicates and sort
-all_words = sorted(set(all_words))#enlever les doublons
-tags = sorted(set(tags))#trier les mots
+all_words = sorted(set(all_words))  # enlever les doublons
+tags = sorted(set(tags))  # trier les mots
 
 print(len(xy), "patterns")
 print(len(tags), "tags:", tags)
 print(len(all_words), "unique stemmed words:", all_words)
 
 # create training data
-X_train = [] 
+X_train = []
 y_train = []
 for (pattern_sentence, tag) in xy:
     # X: bag of words for each pattern_sentence(le mot qu'on rentre)
     bag = bag_of_words(pattern_sentence, all_words)
     X_train.append(bag)
     # y: PyTorch CrossEntropyLoss needs only class labels, not one-hot
-    label = tags.index(tag)#il essaie de trouver l'indice ou se trouve le tag.
+    label = tags.index(tag)  # il essaie de trouver l'indice ou se trouve le tag.
     y_train.append(label)
 
 X_train = np.array(X_train)
 y_train = np.array(y_train)
 
-# Hyper-parameters 
+# Hyper-parameters
 num_epochs = 1000
 batch_size = 8
 learning_rate = 0.001
-input_size = len(X_train[0])#taille de chaque element en entré
+input_size = len(X_train[0])  # taille de chaque element en entré
 hidden_size = 8
 output_size = len(tags)
 print(input_size, output_size)
 
-class ChatDataset(Dataset):
 
+class ChatDataset(Dataset):
     def __init__(self):
         self.n_samples = len(X_train)
         self.x_data = X_train
@@ -77,14 +77,14 @@ class ChatDataset(Dataset):
     def __len__(self):
         return self.n_samples
 
-dataset = ChatDataset()
-#charger toutes nos données.
-train_loader = DataLoader(dataset=dataset,
-                          batch_size=batch_size,
-                          shuffle=True,
-                          num_workers=0)
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+dataset = ChatDataset()
+# charger toutes nos données.
+train_loader = DataLoader(
+    dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=0
+)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 #
 model = NeuralNet(input_size, hidden_size, output_size).to(device)
 
@@ -94,37 +94,37 @@ optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 # Train the model
 for epoch in range(num_epochs):
-    for (words, labels) in train_loader:#decompresser les mots et leurs étiquettes
+    for (words, labels) in train_loader:  # decompresser les mots et leurs étiquettes
         words = words.to(device)
         labels = labels.to(dtype=torch.long).to(device)
-        
+
         # Forward pass
         outputs = model(words)
         # if y would be one-hot, we must apply
         # labels = torch.max(labels, 1)[1]
         loss = criterion(outputs, labels)
-        
+
         # Backward and optimize
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        
-    if (epoch+1) % 100 == 0:
-        print (f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+
+    if (epoch + 1) % 100 == 0:
+        print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}")
 
 
-print(f'final loss: {loss.item():.4f}')
-#sauvegarde de certaines données comme notre model
+print(f"final loss: {loss.item():.4f}")
+# sauvegarde de certaines données comme notre model
 data = {
-"model_state": model.state_dict(),
-"input_size": input_size,
-"hidden_size": hidden_size,
-"output_size": output_size,
-"all_words": all_words,
-"tags": tags
+    "model_state": model.state_dict(),
+    "input_size": input_size,
+    "hidden_size": hidden_size,
+    "output_size": output_size,
+    "all_words": all_words,
+    "tags": tags,
 }
 
 FILE = "data.pth"
-torch.save(data, FILE) #sauvegarde dans un fichier
+torch.save(data, FILE)  # sauvegarde dans un fichier
 
-print(f'training complete. file saved to {FILE}')
+print(f"training complete. file saved to {FILE}")
