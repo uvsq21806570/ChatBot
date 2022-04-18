8c4b361c1ba26a617a365ffa3fab1a9e7758efce
uvsq21806570 <matteo.giraudo@ens.uvsq.fr>
2022-04-18 11:19:33 +0200

    Converting print to string

--- src/chatbot/query_db.py
+++ src/chatbot/query_db.py
@@ -15,30 +15,34 @@ DAY = "DATEPART(weekday, (DATEADD(second, convert(INT, timestamp), '1970-01-01')
 
 
 def current_data(row) :
-    return (str(row[0]) + " " + str(row[2]) + " " + str(row[3]) + " " + str(row[4]) + " " 
-        + str(row[5]) + " " + str(row[6]) + " " + str(row[7]) + " " + str(row[8]) + " " 
-        + str(row[9]) + " " + str(row[10])
+    return (str(row[0]) + ": AQI=" + str(row[2]) + ",  CO=" + str(row[3]) + ", NO=" + str(row[4])
+        + ", NO2=" + str(row[5]) + ",  O3=" + str(row[6]) + ", SO2=" + str(row[7]) + ", NH3=" 
+        + str(row[8]) + ", PM2.5="  + str(row[9]) + ", PM10=" + str(row[10]) + "\n"
     )
     
 
 # tag = function name
 
 def recent_news(cursor) :
+    response = ""
     request = "SELECT * FROM Pollution WHERE timestamp <= " + NOW + " AND timestamp > " + NOW + " - 3600"
     cursor.execute(request)
     row = cursor.fetchone()
     while row:
-        current_data(row)
+        response += current_data(row)
         row = cursor.fetchone()
+    return response
 
 
 def loc_ranking(cursor) :
+    response = "AQI moyen des villes:"
     request = "SELECT loc, AVG(aqi) aqi FROM Pollution GROUP BY loc ORDER BY aqi DESC"
     cursor.execute(request)
     row = cursor.fetchone()
     while row:
-        print(str(row[0]) + " : " + str(row[1]))
+        response += "\n" + str(row[0]) + " : " + str(row[1])
         row = cursor.fetchone()
+    return response
 
 
 # Ville la plus polluée
@@ -46,21 +50,24 @@ def loc_ranking(cursor) :
 
  
 def hour_ranking(cursor) :
+    response = "AQI moyen en fonction du temps:"
     request = "SELECT " + HOUR + " hour, AVG(aqi) aqi FROM Pollution GROUP BY " + HOUR + " ORDER BY hour"
     cursor.execute(request)
     row = cursor.fetchone()
     while row:
-        print(str(row[0]) + " heures : " + str(row[1]))
+        response += "\n" + str(row[0]) + "h : " + str(row[1])
         row = cursor.fetchone()
-
+    return response
 
 def day_ranking(cursor) :
+    response = "AQI moyen en fonction du jour de la semaine:"
     request = "SELECT " + DAY + " day, AVG(aqi) aqi FROM Pollution GROUP BY " + DAY + " ORDER BY day"
     cursor.execute(request)
     row = cursor.fetchone()
     while row:
-        print(str(row[0]) + " " + calendar.day_name[int(row[0]) - 1] + " : " + str(row[1]))
+        response += "\n" + calendar.day_name[int(row[0]) - 1] + " : " + str(row[1])
         row = cursor.fetchone()
+    return response
 
 
 def max_pollution_hour(cursor) :
@@ -69,8 +76,7 @@ def max_pollution_hour(cursor) :
     cursor.execute(request)
     row = cursor.fetchone()
     while row:
-        print("Le pic de pollution est souvent atteint vers " + str(row[0]) + " heures avec un AQI de " + str(row[1]))
-        row = cursor.fetchone()
+        return "Le pic de pollution est souvent atteint vers " + str(row[0]) + "h avec un AQI de " + str(row[1])
 
 
 def min_pollution_hour(cursor) :
@@ -79,8 +85,7 @@ def min_pollution_hour(cursor) :
     cursor.execute(request)
     row = cursor.fetchone()
     while row:
-        print("La pollution est au plus faible vers " + str(row[0]) + " heures avec un AQI de " + str(row[1]))
-        row = cursor.fetchone()
+        return "La pollution est au plus faible vers " + str(row[0]) + "h avec un AQI de " + str(row[1])
 
 
 def max_pollution_day(cursor) :
@@ -89,8 +94,8 @@ def max_pollution_day(cursor) :
     cursor.execute(request)
     row = cursor.fetchone()
     while row:
-        print("Le pic de pollution est souvent atteint le " + calendar.day_name[int(row[0]) - 1] + " avec un AQI de " + str(row[1]))
-        row = cursor.fetchone()
+        return "Le pic de pollution est souvent atteint le " + calendar.day_name[int(row[0]) - 1] + " avec un AQI de " + str(row[1])
+        
 
 
 def min_pollution_day(cursor) :
@@ -99,38 +104,40 @@ def min_pollution_day(cursor) :
     cursor.execute(request)
     row = cursor.fetchone()
     while row:
-        print("La pollution est au plus faible le " + calendar.day_name[int(row[0]) - 1] + " avec un AQI de " + str(row[1]))
-        row = cursor.fetchone()
+        return "La pollution est au plus faible le " + calendar.day_name[int(row[0]) - 1] + " avec un AQI de " + str(row[1])
 
 
 def wrong_place_wrong_time(cursor) :
-    request = "SELECT DISTINCT loc, " + HOUR + " hour, AVG(aqi) aqi FROM Pollution GROUP BY loc, " + HOUR + " HAVING AVG(aqi) >= 3"
+    response = "L'indice de qualité d'air est mauvais à :"
+    request = "SELECT DISTINCT loc, " + HOUR + " hour, AVG(aqi) aqi FROM Pollution GROUP BY loc, " + HOUR + " HAVING AVG(aqi) >= 3 ORDER BY loc"
     cursor.execute(request)
     row = cursor.fetchone()
     while row:
-        print(str(row[0]) + " à " + str(row[1]) + " heures : " + str(row[2]))
+        response += "\n" + str(row[0]) + " à " + str(row[1]) + "h : AQI=" + str(row[2])
         row = cursor.fetchone()
+    return response
 
 
 def wrong_place_wrong_date(cursor) :
+    response = "L'indice de qualité d'air était très mauvais (AQI=5) à :"
     request = "SELECT  loc, timestamp, aqi FROM Pollution WHERE aqi = 5"
     cursor.execute(request)
     row = cursor.fetchone()
-    print("AQI = 5 :")
     while row:
         date = dt.datetime.fromtimestamp(int(row[1]))
-        print(str(row[0]) + " -> " + str(date))
+        response += "\n" + str(row[0]) + " -> " + str(date)
         row = cursor.fetchone()
-
+    return response
 
 def versailles_current_pollution(cursor) :
+    response = ""
     request = "SELECT * FROM Pollution WHERE loc = 'Versailles' AND timestamp <= " + NOW + " AND timestamp > " + NOW + " - 3600"
     cursor.execute(request)
     row = cursor.fetchone()
     while row:
-        current_data(row)
+        response += current_data(row)
         row = cursor.fetchone()
-
+    return response
 
 def lille_current_pollution(cursor) :
     request = "SELECT * FROM Pollution WHERE loc = 'Lille' AND timestamp <= " + NOW + " AND timestamp > " + NOW + " - 3600"
@@ -191,4 +198,4 @@ if __name__ == "__main__":
         + password
     ) as conn:
         with conn.cursor() as cursor:
-            wrong_place_wrong_date(cursor)
+            print(wrong_place_wrong_time(cursor))
