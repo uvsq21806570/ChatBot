be791a0a52d4876ab9add5016e36eb1b95dd55d6
uvsq21806570 <matteo.giraudo@ens.uvsq.fr>
2022-04-23 18:08:57 +0200

    Modified queries

--- src/chatbot/chat.py
+++ src/chatbot/chat.py
@@ -8,10 +8,10 @@ from nltk_utils import bag_of_words, tokenize
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-with open('intents.json', 'r') as json_data:
+with open('./src/chatbot/intents.json', 'r') as json_data:
     intents = json.load(json_data)
 
-FILE = "data.pth"
+FILE = "./src/chatbot/data.pth"
 data = torch.load(FILE)
 
 #informations
